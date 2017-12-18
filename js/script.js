f9cd0befbcd7b3f10385f2b960d9574db7b5b3d5
Monserrat <monserratdiaz12@gmail.com>
2017-12-18 23:04:10 +0000

    functionality

--- js/script.js
+++ js/script.js
@@ -3,46 +3,45 @@
 $(document).ready(function(){
 
         var userChoice;
-        var computerChoice = Math.floor(Math.random() * 3); 
+
         var winner;
         var choices = ["rock", "paper", "scissors"];
-        var finalCompChoice = choices[computerChoice];
+
         var userPoints = 0;
 
-console.log(finalCompChoice);
 
         function notValid(){
                 alert("Invalid Input, try again");
                 $("input[type=text], textarea").val("");
         }
         
-        function RPS() {
+        function RPS(finalCompChoice) {
                 if(finalCompChoice===userChoice){
-                        $("#result").append("DRAW");
+                        $("#result").html("DRAW");
                         winner = "none";
                 }
                 else if(finalCompChoice==="rock" && userChoice==="scissors"){
-                        $("#result").append("you lose");
+                        $("#result").html("you lose");
                         winner = "computer";
                 }
                 else if(finalCompChoice==="rock" && userChoice==="paper"){
-                        $("#result").append("you win");
+                        $("#result").html("you win");
                         winner = "user";
                 }
                 else if(finalCompChoice==="paper" && userChoice==="scissors"){
-                        $("#result").append("you win");
+                        $("#result").html("you win");
                         winner = "user";
                 }
                 else if(finalCompChoice==="paper" && userChoice==="rock"){
-                        $("#result").append("you lose");
+                        $("#result").html("you lose");
                         winner = "computer";
                 }
                 else if(finalCompChoice==="scissors" && userChoice==="rock"){
-                        $("#result").append("you win");
+                        $("#result").html("you win");
                         winner = "user";
                 }
                 else if(finalCompChoice==="scissors" && userChoice==="paper"){
-                        $("#result").append("you lose");
+                        $("#result").html("you lose");
                         winner = "computer";
                 }
         }
@@ -55,11 +54,15 @@ console.log(finalCompChoice);
         }
         
         function displayResult(){
+                $("#index").hide();
                 if(winner==="user"){
-                        window.location = "youWin.html";
+                        $("#youWon").show();
                 }
                 else if(winner==="computer"){
-                        window.location = "youLose.html";
+                        $("#youLost").show();
+                }
+                else if (winner==="none"){
+                        $("#Draw").show();
                 }
         }
         
@@ -72,61 +75,32 @@ console.log(finalCompChoice);
 
         
         $("#shoot").click(function(){
+                var computerChoice = Math.floor(Math.random() * 3);
+                var finalCompChoice = choices[computerChoice];
                 userChoice=$("#userInput").val();
                 userChoice = userChoice.toString().toLowerCase();
                 if(userChoice!=="rock" && userChoice!== "paper" && userChoice!== "scissors"){
                         notValid();
                 }
                 else{
-                $("#yourMove").append(userChoice);
-                $("#computerMove").append(finalCompChoice);
-                RPS();
-                points();
-                $("#counter").val(userPoints);
-
+                  $(".yourMove").html(userChoice);
+                  $(".computerMove").html(finalCompChoice);
+                  RPS(finalCompChoice);
+                  points();
+                 
+                  $(".counter").val(userPoints);
+                  displayResult();
                 }
-                displayResult().delay(4000);
+                
+                
+                
+        });
+        
+        $(".again").click(function() {
+            $("#index").show();
+            $("#youWon, #youLost, #Draw").hide();
+            $("#userInput").val("");
         });
 
 
 });
-
-   
-   
-
-
-   
-   
-        
-
-
-//Class 1:
-        // Fork and clone the repo and set up your project workspace and link files
-        // Write HTML for your project
-//Class 2:  
-        // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
-        // Practice using console.log() and debugger to debug your code
-        // Create your document ready function. 
-        // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
-        // Display the user choice to the output screen
-//Class 3:  
-        // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
-        // Display the computer choice to the output screen
-        // Compare the user choice and the computer choice to determine who won. 
-        // Display the user winner to the output screen 
-        // Clear the input box for the next choice
-
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-//GLOBAL VARIABLES
-
-
-
-//FUNCTIONS
-
-
-
-// DOCUMENT READY FUNCTION
-
